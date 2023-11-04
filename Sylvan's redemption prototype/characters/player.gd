5e80205fe291d2d884867f508b850bcb672fedeb
Anjali Narang <91150763+aknarang03@users.noreply.github.com>
2023-11-03 21:53:11 -0400

    extended level, zoomed out, and incorporated jump animation

--- Sylvan's redemption prototype/characters/player.gd
+++ Sylvan's redemption prototype/characters/player.gd
@@ -2,23 +2,32 @@ extends CharacterBody2D
 
 
 @export var speed : float = 300.0
-@export var jump_velocity : float = -400.0
+@export var jump_velocity : float = -300.0
+@export var float_velocity : float = -300.0
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2 = Vector2.ZERO
+var animation_locked : bool = false
+var was_in_air : bool = false
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+		was_in_air = true
+	else: 
+		if was_in_air == true:
+			land()
+		was_in_air = false
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -31,8 +40,6 @@ func _physics_process(delta):
 	move_and_slide()
 	update_animation()
 	update_facing_direction()
-	
-	
 
 func update_facing_direction():
 	if direction.x > 0:
@@ -42,8 +49,28 @@ func update_facing_direction():
 		
 
 func update_animation():
-	if direction.x != 0:
-		animated_sprite.play("walk")
-	else:
-		animated_sprite.play("idle")
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("walk")
+		else:
+			animated_sprite.play("idle")
 	
+## possibly change it so that velocity depends on how long you hold down space 
+func jump():
+	velocity.y = jump_velocity
+	animated_sprite.play("jump")
+	animation_locked = true
+	
+func glide():
+	## doesn't do anything yet; need to somehow make it so once he reaches full jump height he falls slower
+	velocity.y = float_velocity
+	
+	
+func land():
+	animated_sprite.play("land")
+	animation_locked = true
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "land":
+		animation_locked = false
+		
