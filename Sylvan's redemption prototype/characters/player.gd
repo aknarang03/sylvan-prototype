d8284a9b6f6085d9b70d9aac8515ad09758aae96
Anjali Narang <91150763+aknarang03@users.noreply.github.com>
2023-11-06 12:16:52 -0500

    timer for possession (5 seconds)

--- Sylvan's redemption prototype/characters/player.gd
+++ Sylvan's redemption prototype/characters/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 @export var float_velocity : float = -300.0
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer : Timer = $Timer
+@onready var canvas : CanvasLayer = $CanvasLayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,6 +15,8 @@ var direction : Vector2 = Vector2.ZERO
 var animation_locked : bool = false
 var was_in_air : bool = false
 var possess : bool = false
+var time_possessed : float = 0
+var time_since_possess : float = 0
 @onready var enemy = $"../Enemy"
 
 
@@ -20,6 +24,13 @@ func _physics_process(delta):
 	
 	#print(possess)
 	# Add the gravity.
+	
+	print(timer.time_left)
+	if timer.time_left == 0 and possess == true:
+		animation_locked = false
+		possess = false
+		enemy.show()
+		enemy.set_position(position)
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -39,6 +50,7 @@ func _physics_process(delta):
 		
 		
 	if Input.is_key_label_pressed(KEY_P):
+		#animated_sprite.play("possess_ground")
 		if !possess:
 			#print(position.x)
 			#print(enemy.get_global_pos().x)
@@ -46,6 +58,7 @@ func _physics_process(delta):
 			#print(distance)
 			if abs(distance) < 100:
 				on_possess()
+				#time_possessed = get_physics_process_delta_time()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -106,10 +119,15 @@ func _on_animated_sprite_2d_animation_finished():
 		animation_locked = false
 
 func on_possess():
-	animation_locked = true
-	animated_sprite.play("possess_ground")
-	animation_locked = false
+	#animation_locked = true
+	#animated_sprite.play("possess_ground")
+	#animation_locked = false
 	#animated_sprite.play("bird_idle")
 	possess = true
 	position = enemy.get_global_position()
 	enemy.hide()
+	timer.one_shot = true
+	timer.start(5)
+
+func get_global_pos():
+	return position
