6f8f6519f1f560bbaec360d06ffc8aaab5e54585
Anjali Narang <91150763+aknarang03@users.noreply.github.com>
2023-11-06 21:14:04 -0500

    ray from sylvan to bird when possessing; WASD functionality

--- Sylvan's redemption prototype/characters/player.gd
+++ Sylvan's redemption prototype/characters/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 @onready var timer : Timer = $Timer
-#@onready var canvas : CanvasLayer = $CanvasLayer
+@onready var line : Line2D = $Line2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2 = Vector2.ZERO
@@ -15,36 +15,31 @@ var possess : bool = false
 var time_possessed : float = 0
 var time_since_possess : float = 0
 var falling : bool = false
+var timer_set : bool = false
+var possess_anim_finished : bool = false
 @onready var enemy = $"../Enemy"
-
 
 func _physics_process(delta):
 	
-	#print(possess)
-	# Add the gravity.
-	
 	print(timer.time_left)
+	possess_system()
+	
 	if timer.time_left == 0 and possess == true:
 		animation_locked = false
 		possess = false
 		enemy.show()
 		enemy.set_position(position)
 		if not is_on_floor():
-			#animated_sprite.play("jump") #doesnt work- does it lock when i say possess = false?
-			falling = true
 			animation_locked = false
-			animated_sprite.play("fall")
-	
+			
 	if not is_on_floor():
 		print(velocity.y)
 		velocity.y += gravity * delta
 		was_in_air = true
 		if velocity.y > -15 and velocity.y < 1:
-			#print("velocity is 0")
-			# fix it so that it remains like this for the rest of the fall
 			falling = true
 		if falling == true:
-			if Input.is_action_pressed("ui_accept"):
+			if Input.is_action_pressed("jump"):
 				gravity = 50
 			else:
 				gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -57,29 +52,21 @@ func _physics_process(delta):
 		was_in_air = false
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("jump"):
 		if not possess:
 			if is_on_floor():
 				jump()
 		if possess:
 			jump()
 		
-		
 	if Input.is_key_label_pressed(KEY_P):
-		#animated_sprite.play("possess_ground")
 		if !possess:
-			#print(position.x)
-			#print(enemy.get_global_pos().x)
 			animation_locked = false
 			var distance = sqrt(pow(position.x-enemy.get_global_pos().x,2)+pow(position.y-enemy.get_global_pos().y,2))
-			#print(distance)
 			if abs(distance) < 100:
 				on_possess()
-				#time_possessed = get_physics_process_delta_time()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+				
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
 		velocity.x = direction.x * speed
 	else:
@@ -110,7 +97,6 @@ func update_animation():
 			else:
 				animated_sprite.play("bird_idle")
 	
-## possibly change it so that velocity depends on how long you hold down space 
 func jump():
 	velocity.y = jump_velocity
 	if not possess:
@@ -132,15 +118,21 @@ func _on_animated_sprite_2d_animation_finished():
 		animation_locked = false
 
 func on_possess():
-	#animation_locked = true
-	#animated_sprite.play("possess_ground")
-	#animation_locked = false
-	#animated_sprite.play("bird_idle")
+	line.add_point(to_local(position))
+	line.add_point(to_local(enemy.get_global_position()))
 	possess = true
-	position = enemy.get_global_position()
-	enemy.hide()
-	timer.one_shot = true
-	timer.start(5)
+	possess_anim_finished = false
+	animated_sprite.play("possess_ground")
+	timer_set = false
+	
+func possess_system():
+	if possess and (timer_set == false):
+		position = enemy.get_global_position()
+		enemy.hide()
+		timer.one_shot = true
+		timer_set = true
+		timer.start(5)
+		line.clear_points()
 
 func get_global_pos():
 	return position
